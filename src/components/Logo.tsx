0252b2681f055633c6c37538bfd80e3a538bb76d
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-10-23 17:01:48 +0600

    refactor: update ESLint configuration for consistency and improve Logo component formatting fix: change toast notification to loading state in AuthForm during redirect

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Logo = () => {
   return (
     <Link href="/">
-      <Image alt="Site Icon" src="/site-icon.png" />
+      <Image alt="Site Icon" src="/site-icon.png" />{' '}
     </Link>
   );
 };
