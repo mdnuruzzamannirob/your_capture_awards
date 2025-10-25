5fc1806541c2a903fb6b59a8caf9d783549d856d
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-10-25 17:04:14 +0600

    update user menu comp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // allows any HTTPS domain
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // allows any HTTP domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
