dcf729f719c720c717254f84888ebc017743888e
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-11-09 17:46:04 +0600

    update completed contest card

--- src/utils/getProgress.ts
+++
@@ -1,9 +0,0 @@
-export const getProgress = (length: number, index: number) => {
-  if (length === 1) return 100; // edge case: only 1 user
-
-  const min = 20; // last = 20%
-  const max = 100; // first = 100%
-
-  const value = max - (index / (length - 1)) * (max - min);
-  return value; // 100 → 20
-};
