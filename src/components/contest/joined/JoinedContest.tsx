23716aba1cea5bff479671e9f2d334dcd66cf966
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-11-11 17:21:09 +0600

    complete upload modal

--- src/components/contest/joined/JoinedContest.tsx
+++ src/components/contest/joined/JoinedContest.tsx
@@ -4,8 +4,19 @@ import { useGetJoinedContestQuery } from '@/store/features/contest/contestApi';
 import JoinedContestCard from './JoinedContestCard';
 import JoinedContestCardSkeleton from './JoinedContestCardSkeleton';
 import Image from 'next/image';
+import { useSearchParams } from 'next/navigation';
+import { Dialog, DialogContent, DialogTitle } from '@/components/ui/dialog';
+import { useState } from 'react';
+import { labels, totalLevels } from '@/utils/valueToExposureLabel';
+import { cn } from '@/utils/cn';
 
 const JoinedContest = () => {
+  const searchParams = useSearchParams();
+  const joinSuccess = searchParams.get('modal');
+  const contestId = searchParams.get('contestId');
+  const contestTitle = searchParams.get('contestTitle');
+  const [uploadModal, setUploadModal] = useState(joinSuccess === 'joinSuccess' ? true : false);
+
   const { data, isLoading } = useGetJoinedContestQuery();
 
   const joinedResult = (data as any)?.data ?? [];
@@ -25,6 +36,77 @@ const JoinedContest = () => {
           ))
         )}
       </div>
+      <Dialog open={uploadModal} onOpenChange={setUploadModal}>
+        <DialogContent className="border-black-2-600 border-2 sm:max-w-2xl">
+          <DialogTitle />
+
+          <div className="space-y-5">
+            {/* header */}
+            <h1 className="text-center text-lg font-semibold uppercase sm:text-xl">
+              YOU HAVE JOINED <span className="text-primary">&apos;{contestTitle}&apos;</span>{' '}
+              CHALLENGE
+            </h1>
+
+            {/* content */}
+            <div className="space-y-5">
+              {' '}
+              <div className="flex flex-col items-center justify-center gap-2">
+                <div className="text-muted-foreground text-xs uppercase">Exposure</div>
+
+                <div className="border-black-2-600 relative flex size-[100px] flex-col items-center justify-center rounded-full border-4">
+                  <div className="flex w-full justify-between px-3 text-[10px] text-gray-400">
+                    {labels.map((l, i) => (
+                      <span key={i} className={cn(i + 1 <= 0 && 'font-semibold text-[#FD8533]')}>
+                        {l}
+                      </span>
+                    ))}
+                  </div>
+
+                  <div className="flex gap-0.5">
+                    {Array.from({ length: totalLevels }).map((_, i) => {
+                      const active = i + 1 <= 0;
+                      return (
+                        <div
+                          key={i}
+                          className={cn(
+                            'h-1.5 w-3.5 rounded transition',
+                            active ? 'bg-[#FD8533]' : 'bg-white/20',
+                          )}
+                        ></div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <p className="text-center">
+                Your Exposure Meter is empty. <br /> Fill up to get your Exposure Bonus!
+              </p>
+            </div>
+
+            {/* footer */}
+            <div className="border-black-2-500 flex items-center justify-center gap-10 border-t-[0.5px] pt-5">
+              <button
+                onClick={() => {
+                  // resetStates();
+                }}
+                className="text-primary border-primary rounded-sm border px-5 py-2 text-sm"
+              >
+                Fill
+              </button>
+              <button
+                onClick={() => {
+                  // resetStates();
+                  // handleSubmit()
+                }}
+                // disabled={!file || isLoading}
+                className="bg-primary text-background rounded-sm px-5 py-2 text-sm"
+              >
+                {isLoading ? 'Voting...' : 'Vote'}
+              </button>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </section>
   );
 };
