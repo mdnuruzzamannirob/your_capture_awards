5349f4842a5673bb8a18853414207d0bded6934b
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2025-11-19 16:58:03 +0600

    update some issues

--- src/components/contest/completed/CompletedContestCard.tsx
+++ src/components/contest/completed/CompletedContestCard.tsx
@@ -1,21 +1,9 @@
 import { formatDateToDayMonYear } from '@/utils/formatDateToDayMonYear';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
 import { MdOutlineHowToVote } from 'react-icons/md';
 
 const CompletedContestCard = ({ contest }: { contest: any }) => {
-  const [images, setImages] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (Array.isArray(contest?.photos)) {
-      const mapped = contest?.photos.map((img: any) => img?.url).filter(Boolean);
-
-      setImages(mapped);
-    }
-  }, [contest?.photos]);
-
-  console.log(contest);
   return (
     <div className="text-foreground bg-black-2-800 border-black-2-600 flex flex-col gap-5 overflow-hidden rounded-xl border-2 p-3 lg:flex-row">
       <Link href={`/contest/${contest?.id}`} className="relative h-60 rounded-lg md:h-72 lg:flex-1">
@@ -34,29 +22,17 @@ const CompletedContestCard = ({ contest }: { contest: any }) => {
         <h3 className="text-lg font-medium">Achievements</h3>
 
         <div className="flex items-center gap-6">
-          {/* Circle */}
-          <div className="flex h-20 w-20 items-center justify-center rounded-full border border-white/20 text-xs">
-            img
-          </div>
-
-          {/* Star */}
-          <div className="flex h-20 w-20 items-center justify-center rounded-xl border border-white/20 text-xs">
-            img
-          </div>
-
-          {/* Hexagon-like */}
-          <div className="flex h-20 w-20 items-center justify-center rounded-xl border border-white/20 text-xs">
-            img
-          </div>
+          <Image alt="" src={`/icons/top-photo.png`} width={60} height={50} />
+          <Image alt="" src={`/icons/top-photographer.png`} width={60} height={50} />
         </div>
       </div>
 
       <div className="flex flex-1 flex-col justify-between gap-5">
         <div className="grid grid-cols-2 gap-1">
-          {images?.map((item, index) => (
+          {contest?.photos?.map((item: any, index: number) => (
             <div key={index} className="group relative cursor-pointer overflow-hidden rounded-sm">
               <Image
-                src={item}
+                src={item?.url}
                 alt="Uploaded Photo"
                 width={400}
                 height={260}
@@ -64,7 +40,7 @@ const CompletedContestCard = ({ contest }: { contest: any }) => {
               />
               <p className="absolute bottom-2 left-2 flex items-center justify-center gap-1">
                 <MdOutlineHowToVote />
-                {55}
+                {item?.voteCount ?? 0}
               </p>
             </div>
           ))}
