477974fb9c88a3f5c52186c8e70e6aeafe43be95
Md. Nuruzzaman <dev.mdnuruzzaman@gmail.com>
2026-05-16 17:40:16 +0600

    feat: implement team management features including history, leaderboard, and match components

---
+++ src/app/(withLayout)/team/home/page.tsx
@@ -0,0 +1,7 @@
+const TeamHome = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default TeamHome;
---
+++ src/app/(withLayout)/team/page.tsx
@@ -0,0 +1,16 @@
+const Team = () => {
+  return (
+    <main className="margin container space-y-6">
+      <div className="space-y-2">
+        <h1 className="text-4xl">Teams</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iusto, eum corrupti.</p>
+      </div>
+      <div className="space-y-2">
+        <h2 className="text-2xl">Your Teams</h2>
+        <div className="min-h-60 rounded-lg border bg-white p-4"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Team;
